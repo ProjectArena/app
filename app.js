0b5d75258cafb59d773c3fe8e848bed418a15206
Glyne J. Gittens <Jgit121@gmail.com>
2016-10-09 16:56:51 -0400

    Added code to start the app

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const bodyParser = require('body-parser');
-const cookieParser = requre('cookie-parser');
+const cookieParser = require('cookie-parser');
 const exphbs = require('express-handlebars');
 const express = require('express');
 const expressSession = require('express-session');
 const flash = require('connect-flash');
 const methodOverride = require('method-override');
+
+
+const app = express();
+
+app.listen(8000);
+
+app.engine('handlebars', exphbs({
+  layoutsDir: './views',
+  defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/views/`);
+// app.get('/', (req, res) => {
+//   res.send("This is the home page");
+// });
+app.use(require('./controllers/'));
