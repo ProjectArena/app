9929be6d8e1a093f4b2917af1b8a9b55b98b04c1
Ming Yang <mgy209@nyu.edu>
2016-12-04 14:10:17 -0500

    Profiles are successfully redirecting

--- controllers/profile.js
+++ controllers/profile.js
@@ -14,12 +14,16 @@ router.get('/profile', Redirect.ifNotLoggedIn('/login'), (req, res) => {
 
 // Displays their profile page with all their games
 router.get('/profile/:id', Redirect.ifNotLoggedIn('/login'), (req, res) => {
+
+  // Find the user object associated with the id
   models.User.findById(req.params.id).then((user) => {
+    // If it does not exist, redirect to a 404 page with the error
     if (user === null) {
       let error = "Profile does not exist"
       res.redirect('/404', {error: error, user_info: req.user})
     }
 
+    // Find the games associated with this user
     models.GamerDetail.findAllGames(req.params.id).then((games) => {
       res.render('profile', {user: user, games: games, user_info: req.user})
     })
