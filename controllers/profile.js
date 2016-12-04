5596c0d766479b17ce4fd55170ff376cbca3c0ff
Ming Yang <mgy209@nyu.edu>
2016-12-04 12:56:52 -0500

    Adding logic for profile controller

--- controllers/profile.js
+++ controllers/profile.js
@@ -5,11 +5,15 @@ const router    = express.Router()
 const passport  = require('passport')
 const Redirect  = require('../middlewares/redirect')
 
-router.get('/profile', Redirect.ifNotLoggedIn('/login'), function(req, res) {
-    res.render('profile', {user_info: req.user})
+router.get('/profile/:id', Redirect.ifNotLoggedIn('/login'), function(req, res) {
+    User.findById(req.params.id).then(user) => {
+        if (user === null)
+            res.redirect('/404', {user_info: req.user})
+    }
+    res.render('profile/:id', {user_info: req.user})
 })
 
-router.post('/profile', Redirect.ifNotLoggedIn('/login'), function(req, res){
+router.post('/profile/:id', Redirect.ifNotLoggedIn('/login'), function(req, res){
 
 })
 
